902a212161529cee3caacc07d7244ee583ec49ed
Kage Soichiro <k4gepedia@gmail.com>
2020-03-24 22:49:00 +0900

    tokenが取れるようにする

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,3 @@
-const config = require('./.contentful.json')
 export default {
   mode: 'universal',
   /*
@@ -87,8 +86,8 @@ export default {
    **
    */
   env: {
-    CTF_SPACE_ID: config.CTF_SPACE_ID,
-    CTF_BLOG_POST_TYPE_ID: config.CTF_BLOG_POST_TYPE_ID,
-    CTF_CDA_ACCESS_TOKEN: config.CTF_CDA_ACCESS_TOKEN
+    CTF_SPACE_ID: process.env.CTF_SPACE_ID,
+    CTF_BLOG_POST_TYPE_ID: process.env.CTF_BLOG_POST_TYPE_ID,
+    CTF_CDA_ACCESS_TOKEN: process.env.CTF_CDA_ACCESS_TOKEN
   }
 }
